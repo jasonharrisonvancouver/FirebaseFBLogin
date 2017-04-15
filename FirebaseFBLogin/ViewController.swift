f721c000d977e200a7c5ebabd349065215f875b2
John Kenny Muiruri <john.k.muiruri@gmail.com>
2017-04-15 11:06:13 +0300

    initial commit

--- FirebaseFBLogin/ViewController.swift
+++ FirebaseFBLogin/ViewController.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import FacebookLogin
+import Firebase
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
+    
+    /**
+     Called when the button was used to logout.
+     
+     - parameter loginButton: Button that was used to logout.
+     */
+    public func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        
+    }
+    
+    /**
+     Called when the button was used to login and the process finished.
+     
+     - parameter loginButton: Button that was used to login.
+     - parameter result:      The result of the login.
+     */
+    public func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        switch result {
+        case .failed(let error):
+            print("Error \(error)")
+            break
+        case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+            loginFireBase()
+            break
+        default: break
+        
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let lb = LoginButton(readPermissions: [ .email, .publicProfile ])
+        lb.center = view.center
+        lb.delegate = self
+        view.addSubview(lb)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +57,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /**
+        Login to Firebase after FB Login is successful
+    */
+    func loginFireBase() {
+        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+            // ...
+            if let error = error {
+                // ...
+                return
+            }
+        }
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseFBLogin' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseFBLogin
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'FacebookCore'
+  pod 'FacebookLogin'
+  target 'FirebaseFBLoginTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FirebaseFBLoginUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
